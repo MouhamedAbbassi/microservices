9479052d07e5fc063c0ff3ef2190a7be2e7987a1
MouhamedAbbassi <mouhamed.abbassi@esprit.tn>
2023-12-13 13:15:36 +0100

    keycloack

--- GatewayApi/src/main/java/com/example/GatewayApi/SpringCloudConfig.java
+++ GatewayApi/src/main/java/com/example/GatewayApi/SpringCloudConfig.java
@@ -5,22 +5,32 @@ import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.web.reactive.config.EnableWebFlux;
 
 @Configuration
+@EnableWebFlux
 public class SpringCloudConfig {
-
-
 
     @Bean
     public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
-
         return builder.routes()
-
-                .route("candidates", r -> r.path("/candidats/**").uri("http://candidate:8082"))
-                .route("resources", r -> r.path("/resources/**").uri("http://resource:8083"))
+                .route("candidats", r -> r.path("/candidats/**").uri("http://candidats:8082"))
+                .route("resources", r -> r.path("/resources/**").uri("http://resources:8083"))
                 .build();
+    }
 
 
-
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity serverHttpSecurity) {
+        return serverHttpSecurity.csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .authorizeExchange(exchange -> exchange.pathMatchers("/resources/**","/candidats/**")
+                        .permitAll()
+                        .anyExchange().authenticated()
+                ).oauth2ResourceServer((oauth) -> oauth
+                        .jwt(Customizer.withDefaults()))
+                .build();
     }
 }
